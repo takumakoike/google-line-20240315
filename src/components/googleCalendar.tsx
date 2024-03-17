db4837448077e7a258fd4c0372059ff3f2d5d106
takumakoike <takuma.151e@gmail.com>
2024-03-17 14:54:19 +0900

    gpt4

---
+++ src/components/googleCalendar.tsx
@@ -0,0 +1,9 @@
+import syles from "../styles/calendar.css";
+
+export default function showCal() {
+    return (
+        <div>
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FTokyo&bgcolor=%23ffffff&showTitle=0&showPrint=0&showCalendars=0&showTz=0&src=YWVlN2NmMTY3ODAyNzhhZjdjNGJkOThiNmVhMjVlMGY4ODVjMTQwOGRlMjI5MGIxNzY3YTVmZTYzNzk0NTMxZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E"></iframe>
+        </div>
+    )
+}
