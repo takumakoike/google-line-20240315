d8d738ac28b0ab36e3438ed6d8a4ea9696c910f9
takumakoike <takuma.151e@gmail.com>
2024-03-17 14:25:54 +0900

    build

--- src/components/googleCalendar copy.jsx
+++ src/components/googleCalendar copy.jsx
@@ -9,7 +9,7 @@ export default function showCalendar() {
         if(liff.isLoggedIn()){
             return (
                 <div>
-                <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FTokyo&bgcolor=%23ffffff&showTitle=0&showPrint=0&showCalendars=0&showTz=0&src=YWVlN2NmMTY3ODAyNzhhZjdjNGJkOThiNmVhMjVlMGY4ODVjMTQwOGRlMjI5MGIxNzY3YTVmZTYzNzk0NTMxZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                
             </div>
         )
     } else {
