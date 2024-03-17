c1f8ee079ba5890f62a22f049fe38d5d0d52a9cc
takumakoike <takuma.151e@gmail.com>
2024-03-17 09:13:30 +0900

    return

---
+++ src/api/lineLogin.tsx
@@ -0,0 +1,60 @@
+// LIFFに必要な情報の宣言
+import liff from '@line/liff';
+const LIFF_ID = "2004006139-5eWAxRAO";
+
+liff.init({
+    liffId: LIFF_ID,
+});
+
+function LINE_LOGIN() {
+    // LIFFの初期化が完了したら自動でログイン処理を開始
+    liff.ready.then(() => {
+        if(!liff.isLoggedIn()) {
+            liff.login();
+        } else {
+            // ログイン済みの場合はプロフィールを取得
+            _getUserProfile();
+        }
+    });
+}
+
+// ログイン後にユーザープロフィールを取得する関数、非同期処理
+async function _getUserProfile() {
+    const profile = await liff.getProfile();
+    const name = profile.displayName;
+    
+    return (
+        <div>
+            <p>name</p>
+        </div>
+    )
+    
+    
+    
+    
+    
+    // const userElement = document.getElementById("user");
+    // if (userElement) {
+    //     userElement.textContent = name;
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default LINE_LOGIN();
